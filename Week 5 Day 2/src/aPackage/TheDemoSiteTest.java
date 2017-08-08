7c0340a9e9e5ee01dcb508695401ff8d5f9f83aa
KurtAY <Kurt.Austen-Yembra@qa.com>
2017-08-08 10:38:27 +0100

    Complete

--- Week 5 Day 2/src/aPackage/TheDemoSiteTest.java
+++ Week 5 Day 2/src/aPackage/TheDemoSiteTest.java
@@ -1,14 +1,16 @@
 package aPackage;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TheDemoSiteTest {
-	
 	private WebDriver webDriver;
 	String web = "http://www.TheDemoSite.co.uk";
 	String userName = "user";
@@ -21,29 +23,20 @@ public class TheDemoSiteTest {
 
 	@Test
 	public void Test() {
-		
 		webDriver.manage().window().maximize(); //maximises page so all options can be seen
 		webDriver.navigate().to(web); //opens the web site 
 		
 		//Add user
 		addUser();
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//slow process down
+		waitingTime();
 		
 		//login with added user
 		userLogin();
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//slow process down
+		waitingTime();
 		
 	}
 
@@ -52,43 +45,29 @@ public class TheDemoSiteTest {
 		webDriver.quit();
 	}
 
+	
 	public void addUser() {
 		webDriver.findElement(By.cssSelector("body > div > center > table > tbody > tr:nth-child(2) > "
 				+ "td > div > center > table > tbody > tr > td:nth-child(2) >"
 				+ " p > small > a:nth-child(6)")).click(); //clicks button to add user
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		waitingTime();
 		
 		webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > "
 				+ "center > table > tbody > tr > td:nth-child(1) > div > center > table > "
 				+ "tbody > tr:nth-child(1) > td:nth-child(2) > p > input")).sendKeys(userName); //type the desired user name
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		waitingTime();
 		
 		webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > "
 				+ "center > table > tbody > tr > td:nth-child(1) > div > center > table > "
 				+ "tbody > tr:nth-child(2) > td:nth-child(2) > p > input[type=\"password\"]")).sendKeys(userPass); //types the desired pass
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		waitingTime();
 		
 		webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > "
 				+ "center > table > tbody > tr > td:nth-child(1) > div > center > table > "
-				+ "tbody > tr:nth-child(3) > td:nth-child(2) > p > input[type=\"button\"]")).click();
+				+ "tbody > tr:nth-child(3) > td:nth-child(2) > p > input[type=\"button\"]")).click(); //clicks the save user button
 	}
 	
 	public void userLogin() {
@@ -96,38 +75,36 @@ public class TheDemoSiteTest {
 				+ "td > div > center > table > tbody > tr > td:nth-child(2) > p > "
 				+ "small > a:nth-child(7)")).click(); //clicks the login option on home page
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		waitingTime();
 		
 		webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > tbody "
 				+ "> tr > td:nth-child(1) > table > tbody > "
 				+ "tr:nth-child(1) > td:nth-child(2) > p > input")).sendKeys(userName); //types the user name into selected field
 	
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		waitingTime();
 		
 		webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > "
 				+ "tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) >"
 				+ " td:nth-child(2) > p > input[type=\"password\"]")).sendKeys(userPass); //types the password into the selected field
 		
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		waitingTime();
 		
 		webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > "
 				+ "tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > "
 				+ "td:nth-child(2) > p > input[type=\"button\"]")).click(); //click the "test login" number to enter login
 
+		WebElement message = webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > big > blockquote > "
+				+ "blockquote > font > center > b")); //store success message as a variable
+		
+		assertEquals("unsuccessful login", "**Successful Login**", message.getText());
+	}
+	
+	public void waitingTime() {
+		try {
+			Thread.sleep(7000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
